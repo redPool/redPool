a4b3e564bc5130f798ad579d5b3ec45ffd350ca7
José Daniel Vásquez <jose.vasquez@lognllc.com>
2014-09-09 07:04:04 -0600

    Added DVContants

---
+++ DVCustomizer/DVConstants.h
@@ -0,0 +1,20 @@
+//
+//  DVConstants.h
+//  DVCustomizer
+//
+//  Created by José Daniel Vásquez Gómez on 9/9/14.
+//  Copyright (c) 2014 José Daniel Vásquez Gómez. All rights reserved.
+//
+
+#ifndef DVCustomizer_DVConstants_h
+#define DVCustomizer_DVConstants_h
+
+static NSString *kItemName = @"itemName";
+static NSString *kImageColor = @"imageColor";
+static NSString *kLayer = @"layer";
+static NSString *kColor = @"color";
+static NSString *kWidth = @"width";
+static NSString *kRadius = @"radius";
+static NSString *kCustomizationReloadNotification = @"customizationDictionaryChangedNotification";
+
+#endif
