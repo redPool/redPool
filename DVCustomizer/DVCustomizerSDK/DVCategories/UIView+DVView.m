317d386a88c914f4273f2337e572c4fea4529348
Esteban Torres <me@estebantorr.es>
2014-09-08 19:30:20 -0600

    Better organized code and using constants.
    Signed-off-by: Esteban Torres <me@estebantorr.es>

--- DVCustomizer/DVCustomizerSDK/DVCategories/UIView+DVView.m
+++ DVCustomizer/DVCustomizerSDK/DVCategories/UIView+DVView.m
@@ -6,50 +6,75 @@
 //  Copyright (c) 2014 RedPool. All rights reserved.
 //
 
+// Header
 #import "UIView+DVView.h"
-#import "DVCustomizer.h"
-#import <objc/runtime.h>
 
-NSString * const kDVCustomType = @"kDVCustomType";
+// Utilities
+#import "DVCustomizer.h"
+
+// Native Frameworks
+@import ObjectiveC;
+
+// Constants
+#import "DVConstants.h"
+
+/**
+ *	Custom type key for the objective runtime association.
+ *
+ *	@since 1.0.0
+ */
+static void *kDVCustomType = &kDVCustomType;
+
+@interface UIView ()
+
+void dv_layoutSubviews_Imp(id self, SEL _cmd);
+
+@property (strong, nonatomic) id notificationObserver;
+
+@end
 
 @implementation UIView (DVView)
 
 static IMP __original_layoutSubviews_Imp;
 
+#pragma mark Properties
+
+- (void)setDvCustomType:(NSString *)dvCustomType {
+	objc_setAssociatedObject(self, kDVCustomType, dvCustomType, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (NSString *)dvCustomType {
+	return objc_getAssociatedObject(self, kDVCustomType);
+}
+
+
+#pragma mark - Swizzling
+
 void dv_layoutSubviews_Imp(id self, SEL _cmd) {
     __weak typeof(self)weakSelf = self;
     
     if ([DVSettingsViewController shouldShowAtInit]) {
-    
-        [[NSNotificationCenter defaultCenter] addObserverForName:@"customizationDictionaryChangedNotification"
-                                                      object:nil
-                                                       queue:[NSOperationQueue mainQueue]
-                                                  usingBlock:^(NSNotification *note) {
-                                                      __strong __typeof(weakSelf)strongSelf = weakSelf;
-                                                      if (strongSelf) {
-                                                          if ([strongSelf respondsToSelector:@selector(layoutSubviews)]) {
-                                                              [strongSelf layoutSubviews];
-                                                          } else {
-                                                              [[strongSelf superview] layoutSubviews];
-                                                          }
-                                                      }
-                                                  }];
+		// We need to get a hold of the notification observer to avoid leaking memory.
+        [[NSNotificationCenter defaultCenter] addObserverForName:kDVCustomizationDictionaryChangedNotification
+														  object:nil
+														   queue:[NSOperationQueue mainQueue]
+													  usingBlock:^(NSNotification *note) {
+														  __strong __typeof(weakSelf)strongSelf = weakSelf;
+														  if (strongSelf) {
+															  if ([strongSelf respondsToSelector:@selector(layoutSubviews)]) {
+																  [strongSelf layoutSubviews];
+															  } else {
+																  [[strongSelf superview] layoutSubviews];
+															  }
+														  }
+													  }];
     }
-    [[DVCustomizer sharedManager]customizeComponent:self];
+    [[DVCustomizer sharedManager] customizeComponent:self];
     
     ((void( *)(id, SEL))__original_layoutSubviews_Imp)(self, _cmd);
 }
 
-- (void)setDvCustomType:(NSString *)dvCustomType {
-	objc_setAssociatedObject(self, (__bridge const void *)(kDVCustomType), dvCustomType, OBJC_ASSOCIATION_COPY);
-}
-
-- (NSString *)dvCustomType {
-	return objc_getAssociatedObject(self, (__bridge const void *)(kDVCustomType));
-}
-
 + (void)load {
-    
     static dispatch_once_t swizzleLayoutSubviewsToken;
 	dispatch_once(&swizzleLayoutSubviewsToken, ^{
 		Method m = class_getInstanceMethod([self class], @selector(layoutSubviews));
